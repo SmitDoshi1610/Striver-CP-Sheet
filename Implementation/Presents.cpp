59754447c7cca6b3e57864e105eac55d16dbf5ce
SmitDoshi1610 <93754536+SmitDoshi1610@users.noreply.github.com>
2023-06-18 20:19:05 +0530

    Create Presents.cpp

---
+++ Implementation/Presents.cpp
@@ -0,0 +1,47 @@
+// Code By : Smit Doshi
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1e9 + 7
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define INPUT(a, n) for (ll i=0; i<n; i++) cin >> a[i];
+#define INF 1000000000000000003
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+void smit()
+{
+   int n;
+   cin >> n;
+   unordered_map<int,int>mp;
+   for(int i=1; i<=n; i++)
+   {
+      int x;
+      cin >> x;
+      mp[x] = i;
+   }
+   for(int i=1; i<=n; i++)
+   {
+      cout << mp[i] << " ";
+   }
+}
+
+int main()
+{
+      
+      // int t;
+      // cin >> t;
+      // while(t--)
+      // {
+      //    smit();
+      // }
+   smit();   
+      return 0;
+}
